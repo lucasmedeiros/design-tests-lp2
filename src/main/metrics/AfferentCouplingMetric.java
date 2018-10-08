53271b4c043ddefe92a18b48b944b56a5bbc55e4
LukeHxH <lucasmed812@gmail.com>
2018-10-08 15:21:54 -0300

    implementa acoplamento aferente e eferente

--- src/main/metrics/AfferentCouplingMetric.java
+++ src/main/metrics/AfferentCouplingMetric.java
@@ -1,5 +1,79 @@
 package main.metrics;
 
-public class AfferentCouplingMetric {
+import java.util.HashSet;
+import java.util.Set;
 
+import org.designwizard.api.DesignWizard;
+import org.designwizard.design.ClassNode;
+import org.designwizard.design.MethodNode;
+
+public class AfferentCouplingMetric extends AbstractCouplingMetric {
+
+	public AfferentCouplingMetric(DesignWizard dw) {
+		super(dw);
+	}
+
+	@Override
+	public int calculate(ClassNode classNode) {
+		int ca = 0;
+
+		if (classNode != null)
+			ca = getDependentsEntities(classNode).size();
+
+		return ca;
+	}
+
+	private Set<ClassNode> getDependentsEntities(ClassNode classNode) {
+		Set<ClassNode> directDependentsEntities = new HashSet<>();
+
+		directDependentsEntities.addAll(getDirectDependentsEntities(classNode));
+		directDependentsEntities.addAll(getMethodDependentsEntities(classNode));
+
+		return directDependentsEntities;
+	}
+
+	private Set<ClassNode> getDirectDependentsEntities(ClassNode classNode) {
+		Set<ClassNode> feedback = new HashSet<>();
+		Set<ClassNode> callers = classNode.getCallerClasses();
+
+		for (ClassNode caller : callers) {
+			if (isNewRelatedClass(classNode, caller)) {
+				feedback.add(caller);
+			}
+		}
+		return feedback;
+	}
+
+	private Set<ClassNode> getMethodDependentsEntities(ClassNode classNode) {
+		Set<ClassNode> feedback = new HashSet<>();
+
+		Set<ClassNode> callerClasses = classNode.getCallerClasses();
+		for (ClassNode callerClass : callerClasses) {
+			Set<MethodNode> methods = callerClass.getAllMethods();
+
+			for (MethodNode method : methods) {
+				feedback.addAll(getDependentsDeclaringClasses(classNode, method));
+			}
+		}
+		return feedback;
+	}
+
+	private Set<ClassNode> getDependentsDeclaringClasses(ClassNode classNode, MethodNode method) {
+		Set<ClassNode> feedback = new HashSet<>();
+		ClassNode type = method.getReturnType();
+		Set<ClassNode> parameters = getParameters(method);
+
+		if (type.equals(classNode) || parameters.contains(classNode)) {
+			Set<MethodNode> callerMethods = method.getCallerMethods();
+
+			for (MethodNode callerMethod : callerMethods) {
+				ClassNode declaringClass = callerMethod.getDeclaringClass();
+
+				if (isNewRelatedClass(classNode, declaringClass)) {
+					feedback.add(declaringClass);
+				}
+			}
+		}
+		return feedback;
+	}
 }
--- src/main/metrics/EfferentCouplingMetric.java
+++ src/main/metrics/EfferentCouplingMetric.java
@@ -1,5 +1,85 @@
 package main.metrics;
 
-public class EfferentCouplingMetric {
+import java.util.HashSet;
+import java.util.Set;
 
+import org.designwizard.api.DesignWizard;
+import org.designwizard.design.ClassNode;
+import org.designwizard.design.FieldNode;
+import org.designwizard.design.MethodNode;
+
+public class EfferentCouplingMetric extends AbstractCouplingMetric {
+
+	public EfferentCouplingMetric(DesignWizard dw) {
+		super(dw);
+	}
+
+	@Override
+	public int calculate(ClassNode classNode) {
+		int ce = 0;
+
+		if (classNode != null)
+			ce = getRelatedEntities(classNode).size();
+
+		return ce;
+	}
+
+	private Set<ClassNode> getRelatedEntities(ClassNode classNode) {
+		Set<ClassNode> directRelatedEntities = new HashSet<>();
+
+		directRelatedEntities.addAll(getDirectRelatedEntities(classNode));
+		directRelatedEntities.addAll(getMethodRelatedEntities(classNode));
+		directRelatedEntities.addAll(getFieldDeclaredEntities(classNode));
+
+		return directRelatedEntities;
+	}
+
+	private Set<ClassNode> getDirectRelatedEntities(ClassNode classNode) {
+		Set<ClassNode> directedRelatedEntities = new HashSet<>();
+
+		for (ClassNode calleeClass : classNode.getCalleeClasses()) {
+			if (isNewRelatedClass(classNode, calleeClass)) {
+				directedRelatedEntities.add(calleeClass);
+			}
+		}
+		return directedRelatedEntities;
+	}
+
+	private Set<ClassNode> getFieldDeclaredEntities(ClassNode classNode) {
+		Set<ClassNode> feedback = new HashSet<>();
+		Set<FieldNode> fieldsDeclared = classNode.getAllFields();
+
+		for (FieldNode field : fieldsDeclared) {
+			ClassNode type = field.getType();
+			if (isNewRelatedClass(classNode, type)) {
+				feedback.add(type);
+			}
+		}
+
+		return feedback;
+	}
+
+	private Set<ClassNode> getMethodRelatedEntities(ClassNode classNode) {
+		Set<ClassNode> feedback = new HashSet<>();
+
+		Set<MethodNode> methods = new HashSet<>();
+		methods.addAll(classNode.getAllMethods());
+		methods.addAll(classNode.getCalleeMethods());
+
+		for (MethodNode method : methods) {
+			ClassNode type = method.getReturnType();
+			if (isNewRelatedClass(classNode, type)) {
+				feedback.add(type);
+			}
+
+			Set<ClassNode> parameters = getParameters(method);
+
+			for (ClassNode parameter : parameters) {
+				if (isNewRelatedClass(classNode, parameter)) {
+					feedback.add(parameter);
+				}
+			}
+		}
+		return feedback;
+	}
 }
