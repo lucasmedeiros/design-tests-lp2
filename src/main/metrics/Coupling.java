0522cebd91a9d9ef29fbd180ca13ceb69d104371
LukeHxH <lucasmed812@gmail.com>
2018-10-16 23:59:36 -0300

    adiciona classe para os calculos de coeficientes de acoplamento, incluindo instabilidade

---
+++ src/main/metrics/Coupling.java
@@ -0,0 +1,34 @@
+package main.metrics;
+
+import org.designwizard.api.DesignWizard;
+import org.designwizard.design.ClassNode;
+
+public class Coupling {
+	
+	private Metric afferent;
+	private Metric efferent;
+
+	public Coupling (DesignWizard dw) {
+		this.afferent = new AfferentCouplingMetric(dw);
+		this.efferent = new EfferentCouplingMetric(dw);
+	}
+	
+	public double instability(ClassNode classNode) {
+		double ca = (double) getAfferentCoupling(classNode);
+		double ce = (double) getEfferentCoupling(classNode);
+		double instability = 0.0;
+		
+		if (ce > 0.0)
+			instability = ce / (ce + ca);
+		
+		return instability;
+	}
+	
+	public int getAfferentCoupling(ClassNode classNode) {
+		return this.afferent.calculate(classNode);
+	}
+	
+	public int getEfferentCoupling(ClassNode classNode) {
+		return this.efferent.calculate(classNode);
+	}
+}
