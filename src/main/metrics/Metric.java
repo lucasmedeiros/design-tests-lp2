e3e27b90e1cc78fea29f40cf5f674dd117df3fec
LukeHxH <lucasmed812@gmail.com>
2018-10-18 13:43:23 -0300

    documentação Metric.java

--- src/main/metrics/Metric.java
+++ src/main/metrics/Metric.java
@@ -2,7 +2,19 @@ package main.metrics;
 
 import org.designwizard.design.ClassNode;
 
+/**
+ * Calculates the metric value for the {@link ClassNode}
+ * @author Lucas de Medeiros Nunes Fernandes - lucasmed812@gmail.com
+ */
+
 public interface Metric {
 	
+	/**
+	 * Calculates the value of the metric for a given classNode.
+	 * 
+	 * @param classNode class to be used to calculate the metric, which is contained in
+	 * project design.
+	 * @return the value of the metric.
+	 */
 	public int calculate(ClassNode classNode);
 }
