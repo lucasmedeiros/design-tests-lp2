61b7882313fa7f2d273b03a6bea800fd9e3df6d8
LukeHxH <lucasmed812@gmail.com>
2018-10-30 08:37:55 -0300

    inicia experttool e creatormetric

---
+++ src/main/metrics/CreatorMetric.java
@@ -0,0 +1,17 @@
+package main.metrics;
+
+import org.designwizard.design.ClassNode;
+
+public class CreatorMetric implements Metric {
+	
+	public CreatorMetric() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int calculate(ClassNode classNode) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/tools/ExpertTool.java
@@ -0,0 +1,7 @@
+package main.tools;
+
+public class ExpertTool {
+	
+	
+
+}
